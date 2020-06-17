f8879adaeece30e7fcf930306762217283e698ca
Johan Manuel <johan.manuel@live.fr>
2020-06-17 16:33:36 +0200

    Respect shellcheck's advice in toolchain script

--- toolchain/build-toolchain.sh
+++ toolchain/build-toolchain.sh
@@ -4,19 +4,19 @@ MAKE_OPTIONS="-j1"
 
 function download() { # $1=url, $2=file
 	if [ ! -f $2 ]; then
-		curl "$1$2" > $2
+		curl "$1$2" > "$2"
 	fi
 
-	if [ ! -d ${2%.tar.xz} ]; then
-		tar xf $2
+	if [ ! -d "${2%.tar.xz}" ]; then
+		tar xf "$2"
 	fi
 }
 
 function download-binutils() {
 	url="ftp://ftp.gnu.org/gnu/binutils/"
 	file=$(curl -sl $url | grep -E 'binutils-[0-9.]+.tar.xz$' | sort -V | tail -n1)
-	download $url $file
-	echo $PWD/${file%.tar.xz}
+	download "$url" "$file"
+	echo "$PWD/${file%.tar.xz}"
 }
 
 function download-gcc() {
@@ -24,14 +24,14 @@ function download-gcc() {
 	folder=$(curl -sl $url | grep -E 'gcc-[0-9.]+$' | sort -V | tail -n1)
 	url="$url$folder/"
 	file="$folder.tar.xz"
-	download $url $file
-	echo $PWD/$folder
+	download "$url" "$file"
+	echo "$PWD/$folder"
 }
 
 echo "Downloading dependencies"
 
 mkdir -p src
-cd src
+cd src || exit 1
 echo " - binutils"
 binutils_src=$(download-binutils)
 echo " - gcc"
@@ -46,11 +46,11 @@ export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
 mkdir -p build
-cd build
+cd build || exit 2
 
 mkdir -p binutils
-cd binutils
-$binutils_src/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
+cd binutils || exit 3
+"$binutils_src/configure" --target="$TARGET" --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make $MAKE_OPTIONS
 make install
 cd ..
@@ -58,8 +58,8 @@ cd ..
 echo "Building GCC"
 
 mkdir -p gcc
-cd gcc
-$gcc_src/configure --target=$TARGET --prefix=$PREFIX --disable-nls --enable-languages=c,c++ --without-headers
+cd gcc || exit 4
+"$gcc_src/configure" --target="$TARGET" --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc $MAKE_OPTIONS
 make all-target-libgcc $MAKE_OPTIONS
 make install-gcc
