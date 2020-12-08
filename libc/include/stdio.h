3b55ee5bd97c606feb61457edd9bc0cdba67cc61
Johan Manuel <johan.manuel@live.fr>
2020-12-08 18:27:11 +0100

    Port stb_sprintf
    Alright, now we got it all: sprintf, snprintf, fprintf... With all the
    modifiers possible. Except for floats, 4K of code seemed a bit big for
    something I'll never use.
    
    Major props to stb_printf's author ;)

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdarg.h>
 #include <stdint.h>
 #include <unistd.h>
 
@@ -11,13 +12,22 @@ typedef struct {
 } FILE;
 
 extern FILE* stdout;
+extern FILE* stderr;
 
-int printf(const char* __restrict, ...);
 int putchar(int);
 int puts(const char*);
 
+int printf(const char* format, ...);
+int sprintf(char* str, const char* format, ...);
+int snprintf(char* str, size_t size, const char* format, ...);
+int vsprintf(char* str, const char* format, va_list ap);
+int vsnprintf(char* str, size_t size, const char* format, va_list ap);
+
 int rename(const char* old, const char* new);
 
+/* Those write to files, which the kernel better not do through the libc, rather
+ * through its `fs_*` functions.
+ */
 #ifndef _KERNEL_
 FILE* fopen(const char* path, const char* mode);
 int fclose(FILE* stream);
@@ -25,4 +35,6 @@ int fread(void* ptr, size_t size, size_t nmemb, FILE* stream);
 int fgetc(FILE* stream);
 int fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream);
 int fputc(int c, FILE* stream);
+int fprintf(FILE* stream, const char* format, ...);
+int vfprintf(FILE* stream, const char* format, va_list ap);
 #endif
---
+++ libc/src/stdio/stb_sprintf.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#define STB_SPRINTF_IMPLEMENTATION
+#define STB_SPRINTF_NOFLOAT
+#define STB_SPRINTF_NOUNALIGNED
+#define STB_SPRINTF_MIN 512
+#include <deps/stb_sprintf.h>
+
+static FILE __stdout = (FILE) {STDOUT_FILENO, "stdout"};
+
+// No such thing as stderr right now
+FILE* stdout = &__stdout;
+FILE* stderr = &__stdout;
+
+/* Both gcc and clang generate calls to these two during the compilation of
+ * `stb_sprintf` for some reason. Let's not link with -lgcc again.
+ */
+unsigned long __udivdi3(unsigned long a, unsigned long b) {
+    return a / b;
+}
+
+unsigned long __umoddi3(unsigned long a, unsigned long b) {
+    return a % b;
+}
+
+static char* callback(const char* buf, void* fd, int len) {
+    (void) fd;
+
+#ifndef _KERNEL_
+    fwrite(buf, 1, len, fd);
+#endif
+
+    for (int i = 0; i < len; i++) {
+        putchar((int) buf[i]);
+    }
+
+    return (char*) buf;
+}
+
+int sprintf(char* str, const char* format, ...) {
+    va_list ap;
+
+    va_start(ap, format);
+    int ret = vsprintf(str, format, ap);
+    va_end(ap);
+
+    return ret;
+}
+
+int snprintf(char* str, size_t size, const char* format, ...) {
+    va_list ap;
+
+    va_start(ap, format);
+    int ret = vsnprintf(str, size, format, ap);
+    va_end(ap);
+
+    return ret;
+}
+
+int vsprintf(char* str, const char* format, va_list ap) {
+    return stbsp_vsprintf(str, format, ap);
+}
+
+int vsnprintf(char* str, size_t size, const char* format, va_list ap) {
+    return stbsp_vsnprintf(str, size, format, ap);
+}
+
+int vfprintf(FILE* stream, const char* format, va_list ap) {
+    char buf[STB_SPRINTF_MIN];
+    return stbsp_vsprintfcb(callback, (void*) stream, buf, format, ap);
+}
+
+int fprintf(FILE* fd, const char* format, ...) {
+    va_list ap;
+
+    va_start(ap, format);
+    int ret = vfprintf(fd, format, ap);
+    va_end(ap);
+
+    return ret;
+}
+
+int printf(const char* format, ...) {
+    va_list ap;
+
+    va_start(ap, format);
+    int ret = vfprintf(stdout, format, ap);
+    va_end(ap);
+
+    return ret;
+}
