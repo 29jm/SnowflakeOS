e16b9d0a32b60b2287e7db016164c6fdf10485ae
Johan Manuel <johan.manuel@live.fr>
2021-01-12 19:18:00 +0100

    Parts of IDE command set

--- kernel/include/kernel/pci.h
+++ kernel/include/kernel/pci.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <list.h>
 
 typedef struct pci_header_t {
     uint16_t vendor, device;
@@ -10,7 +11,6 @@ typedef struct pci_header_t {
 } __attribute__((packed)) pci_header_t;
 
 typedef struct pci_header0_t {
-    pci_header_t header;
     uint32_t bar[6];
     uint32_t cardbus_cis_addr;
     uint16_t subsystem_vendor, subsystem;
@@ -20,9 +20,57 @@ typedef struct pci_header0_t {
     uint8_t int_line, int_pin, min_grant, max_latency;
 } __attribute__((packed)) pci_header0_t;
 
+/* These are not useable right now: field must be reordered by groups of 4
+ * bytes, reversed. */
+typedef struct pci_header1_t {
+    uint32_t bar[2];
+    uint8_t latency2, sub_bus, sec_bus, prim_bus;
+    uint16_t secondary_status;
+    uint8_t io_limit, io_base;
+    uint16_t memory_limit, memory_base;
+    uint16_t prefetch_limit, prefetch_base;
+    uint32_t prefetch_upper_limit, prefetch_upper_base;
+    uint16_t io_upper_limit, io_upper_base; // TODO use io_limit* prefixes
+    uint8_t reserved[3];
+    uint8_t capabilities_addr;
+    uint32_t expansion;
+    uint16_t bridge_control;
+    uint8_t int_pin, int_line;
+} __attribute__((packed)) pci_header1_t;
+
+typedef struct pci_header2_t {
+    uint32_t carbus_addr;
+    uint16_t sec_status;
+    uint8_t reserved, capabilities_offset;
+    uint8_t cardbus_latency, sub_bus, carbus_bus, pci_bus;
+    uint32_t mem_base_addr0;
+    uint32_t mem_limit0;
+    uint32_t mem_base_addr1;
+    uint32_t mem_limit1;
+    uint32_t io_base_addr0;
+    uint32_t io_limit0;
+    uint32_t io_base_addr1;
+    uint32_t io_limit1;
+    uint16_t bridge_control;
+    uint8_t int_pin, int_line;
+    uint16_t sub_vendor, sub_device;
+    uint32_t legacy_base_addr;
+} __attribute__((packed)) pci_header2_t;
+
 typedef struct pci_device_t {
+    uint8_t id;
     uint8_t bus, dev, func;
-} pci_device_t;
+    uint8_t hdr_type;
+    pci_header_t hdr;
+    union {
+        pci_header0_t header0;
+        pci_header1_t header1;
+        pci_header2_t header2;
+    };
+} __attribute__((packed)) pci_device_t;
+
 
 void init_pci();
 uint32_t pci_read_config(uint8_t bus, uint8_t dev, uint8_t func, uint8_t* buf, uint32_t size);
+void pci_print_device(pci_device_t* dev);
+void pci_print_all_devices(list_t *list);
---
+++ kernel/src/devices/ide.c
@@ -0,0 +1,6 @@
+#include <kernel/ide.h>
+#include <kernel/pci.h>
+
+void init_ide(pci_device_t dev) {
+    return;
+}
